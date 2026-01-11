78d9cfd1d56f975f3d946e731ef0dca7d326e9f0
Vishal Singh <vsvishalrana2020@gmail.com>
2026-01-11 22:24:11 +0530

    file added insert element

--- src/IncDecMix.java
+++ src/IncDecMix.java
@@ -1,16 +1,20 @@
+
+// This is increment decrement code practice
    public class IncDecMix {
     public static void main(String[] args) {
-//        int a = 5, b;
-//
-//        b = ++a + a++ + --a + a--;
-//        System.out.println("a = " + a);
-//        System.out.println("b = " + b);
+        int a = 5, b;
 
-
-        int a = 10;
-        int b = 5;
-        int result = a + b;
-        System.out.println("a + b is = " +result);
-
+        b = ++a + a++ + --a + a--;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
+
+//
+//        int a = 10;
+//        int b = 5;
+//        int result = a + b;
+//        System.out.println("a + b is = " +result);
+//
+//    }
+//}
---
+++ src/InsertElement.java
@@ -0,0 +1,34 @@
+public class InsertElement {
+    public static void main(String[] args) {
+
+        // Original array
+        int[] arr = {10, 20, 30, 40};
+
+        // Insert karne wala element
+        int element = 25;
+
+        // Jis index par insert karna hai
+        int index = 2;
+
+        // New array (size + 1)
+        int[] newArr = new int[arr.length + 1];
+
+        // Index se pehle ke elements copy
+        for (int i = 0; i < index; i++) {
+            newArr[i] = arr[i];
+        }
+
+        // Element insert
+        newArr[index] = element;
+
+        // Baaki elements shift karke copy
+        for (int i = index; i < arr.length; i++) {
+            newArr[i + 1] = arr[i];
+        }
+
+        // 🔹 NORMAL for loop se array print
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.print(newArr[i] + " ");
+        }
+    }
+}
