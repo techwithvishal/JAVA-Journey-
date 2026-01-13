5dd00f07d9d7130321d04a865190da82e80adf84
Vishal Singh <vsvishalrana2020@gmail.com>
2026-01-13 15:54:27 +0530

    polymorphism practice

--- src/LearningJava/Test.java
+++ src/LearningJava/Test.java
@@ -1,7 +1,28 @@
-package LearningJava;
+//package LearningJava;
+//
+//public class Test {
+//    static void main() {
+//        System.out.printf("Kya Hal h");
+//    }
+//}
+;
+//
+//    Practice polymorphism program
+//    Method overloading (compile time polymorphism)
 
-public class Test {
-    static void main() {
-        System.out.printf("Kya Hal h");
+public class Test{
+    int add(int a, int b) {
+        return (a + b);
+
+    }
+    int add(int a , int b, int c ){
+        return (a + b + c );
+    }
+
+
+    public  static void main(String[] args){
+        Test t =  new Test();
+        System.out.println(t.add(4,5,6));
+        System.out.println(t.add(2, 4));
     }
 }
